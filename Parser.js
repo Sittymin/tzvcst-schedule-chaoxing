dfa08e074baff5de7899026dec369564ed891c27
Sittymin <2890108976@qq.com>
2023-11-04 14:15:20 +0800

    先抄写

--- Parser.js
+++ Parser.js
@@ -1,47 +1,305 @@
+/**
+ * 解析课程表
+ *
+ * @link https://github.com/moeshin/ai-schedule-chaoxing
+ * @param html
+ * @returns {[]}
+ */
 function scheduleHtmlParser(html) {
-    //除函数名外都可编辑
-    //传入的参数为上一步函数获取到的html
-    //可使用正则匹配
-    //可使用解析dom匹配，工具内置了$，跟jquery使用方法一样，直接用就可以了，参考：https://cnodejs.org/topic/5203a71844e76d216a727d2e
-    let result = []
-    let bbb = $('#table1 .timetable_con')
-    for (let u = 0; u < bbb.length; u++) {
-      let re = { sections: [], weeks: [] }
-      let aaa = $(bbb[u]).find('span')
-      let week = $(bbb[u]).parent('td')[0].attribs.id
-      if (week) {
-        re.day = week.split('-')[0]
-      }
-      for (let i = 0; i < aaa.length; i++) {
-        if (aaa[i].attribs.title == '上课地点') {
-          for (let j = 0; j < $(aaa[i]).next()[0].children.length; j++) {
-            re.position = $(aaa[i]).next()[0].children[j].data
-          }
-        }
-        if (aaa[i].attribs.title == '节/周') {
-          for (let j = 0; j < $(aaa[i]).next()[0].children.length; j++) {
-            let lesson = $(aaa[i]).next()[0].children[j].data
-            for (let a = Number(lesson.split(')')[0].split('(')[1].split('-')[0]); a < Number(lesson.split(')')[0].split('(')[1].split('-')[1].split('节')[0]) + 1; a++) {
-              re.sections.push({ section: a })
-            }
-            for (let a = Number(lesson.split(')')[1].split('-')[0]); a < Number(lesson.split(')')[1].split('-')[1].split('周')[0]) + 1; a++) {
-              re.weeks.push(a)
-            }
-          }
-        }
-        if (aaa[i].attribs.title == '教师') {
-          for (let j = 0; j < $(aaa[i]).next()[0].children.length; j++) {
-            re.teacher = $(aaa[i]).next()[0].children[j].data
-          }
-        }
-        if (aaa[i].attribs.class == 'title') {
-          for (let j = 0; j < $(aaa[i]).children()[0].children.length; j++) {
-            re.name = $(aaa[i]).children()[0].children[j].data
-          }
-        }
-      }
-      result.push(re)
-    }
-    console.log(result)
-    return result
+  if (html === '<!-- PASS -->') {
+      return [{
+          name: 'PASS',
+          day: 1,
+          position: '',
+          sections: [{
+              section: 1
+          }],
+          weeks: [1]
+      }];
   }
+  let infos = [];
+  if (html.startsWith('<!-- JSON -->')) {
+      infos = JSON.parse(html.substring(13));
+  } else {
+      const tds = $('tbody td.cell');
+      const tdsLen = tds.length;
+      for (let i = 0; i < tdsLen; ++i) {
+          let td = tds[i];
+          const children = td.children;
+          if (!children || children.length === 0) {
+              continue;
+          }
+          const preset = getPreset(td.attribs);
+          let node = children[0];
+          if (node.name === 'br') {
+              node = node.firstChild;
+          }
+          try {
+              parseCell(infos, preset, node);
+          } catch (e) {
+              console.error(i, e, td);
+          }
+      }
+  }
+  infos = format(infos);
+  console.log('infos', infos);
+  return infos;
+}
+
+function assert(b) {
+  if (!b) {
+      console.error('Assert');
+      throw 'Assert';
+  }
+}
+
+/**
+* 获取周
+*
+* 1周
+* 1-2周
+* 1-1,3-3周
+* 1-9(单)周
+* 2-10(双)周
+*
+* @param str {string} 周，例如：1-2周、1周
+* @returns {[]|*[]}
+*/
+function getWeeks(str) {
+  const weeks = [];
+   str = str.trim();
+  if (str.endsWith('周')) {
+      str = str.substring(0, str.length - 1);
+  }
+  for (const w of str.split(',')) {
+      const match = w.match(/^(\d+)(?:-(\d+)(?:\(([单双])\))?)?$/);
+      let i = parseInt(match[1]);
+      if (match[2] === undefined) {
+          weeks.push(i);
+          continue;
+      }
+      const end = parseInt(match[2]) + 1;
+      const state = match[3];
+      if (state === undefined) {
+          for (; i < end; ++i) {
+              weeks.push(i);
+          }
+          continue;
+      }
+      if (i % 2 === 0) {
+          if (state === '单') {
+              ++i;
+          }
+      } else {
+          if (state === '双') {
+              ++i;
+          }
+      }
+      for (; i < end; i += 2) {
+          weeks.push(i);
+      }
+  }
+  return weeks;
+}
+
+/**
+* 获取预设
+*
+* @param attrs
+* @returns {{day: number, sections: []}}
+*/
+function getPreset(attrs) {
+  const sections = [];
+  const match = attrs.id.match(/^Cell(\d)(\d+)$/);
+  let i = parseInt(match[2])
+  const rowspan = attrs.rowspan;
+  const length = i + (rowspan ? parseInt(rowspan): 0);
+  for (; i < length; ++i) {
+      sections.push({
+          section: i
+      });
+  }
+  return {
+      day: parseInt(match[1]),
+      sections: sections
+  }
+}
+
+/**
+* 验证 onclick
+*
+* @param node cheerio 节点
+* @param fun 函数名
+* @returns {*|boolean}
+*/
+function hasOnClick(node, fun) {
+  const attrs = node.attribs;
+  return attrs && attrs.onclick.trim().startsWith(fun + '(');
+}
+
+/**
+* 处理单元
+*
+* @param infos {[]} 全部课程信息
+* @param preset {{}} 预设
+* @param node {{}} 第一个节点
+*/
+function parseCell(infos, preset, node) {
+  const info = Object.assign({
+      position: ''
+  }, preset);
+
+  // debugger
+  {
+      let n;
+      if (node.type === 'text' && (n = node.next) && n.name === 'br' && (n = n.firstChild) && n.type === 'text') {
+          // 过滤课程代码
+          node = n;
+      }
+  }
+  if (node.type === 'text') {
+      // 校区
+      info.position += node.data;
+      assert(node = node.next);
+      assert(node.name === 'br');
+      assert(node = node.firstChild);
+  }
+
+  assert(node.name === 'a');
+  assert(hasOnClick(node, 'openKckb'));
+  info.name = $(node).text();
+
+  assert(node = node.next);
+
+  if (node.type === 'text') {
+      // 课程类型
+      info.name += node.data;
+      assert(node = node.next);
+  }
+
+  assert(node.name === 'br');
+  assert(node = node.firstChild);
+  assert(node.name === 'a');
+  assert(hasOnClick(node, 'openJskb'));
+  info.teacher = $(node).text();
+
+  assert(node = node.next);
+  if (node.name === 'br') {
+      assert(node = node.firstChild);
+  }
+  assert(node.type === 'text');
+  info.weeks = getWeeks(node.data);
+
+  assert(node = node.next);
+  assert(node.name === 'br');
+
+  if ((node = node.firstChild) && node.name === 'a') {
+      assert(hasOnClick(node, 'openCrkb'));
+      if (info.position.length !== 0) {
+          info.position += '-';
+      }
+      info.position += $(node).text();
+      assert(node = node.next);
+      assert(node.name === 'br');
+      node = node.firstChild;
+  }
+
+  infos.push(info);
+
+  if (node && node.type === 'text' && node.data === ' ') {
+      node = node.next
+  }
+
+  if (node) {
+      assert(node.name === 'br');
+      assert(node = node.firstChild);
+      parseCell(infos, preset, node);
+  }
+}
+
+function hash(...args) {
+  return args.join('-');
+}
+
+function sortAsc(a, b) {
+  return a - b;
+}
+
+function setToArr(set) {
+  return Array.from(set).sort(sortAsc);
+}
+
+function format(infos) {
+  const map1 = {};
+  for (const info of infos) {
+      let weeks = info.weeks;
+      const day = info.day;
+      const name = info.name;
+      const teacher = info.teacher;
+      const position = info.position;
+      if (typeof weeks === 'string') {
+          weeks = getWeeks(weeks);
+      }
+      for (const week of weeks) {
+          const k = hash(week, day, name, teacher, position);
+          let obj = map1[k];
+          if (obj === undefined) {
+              map1[k] = obj = {
+                  name,
+                  teacher,
+                  position,
+                  day,
+                  week,
+                  sections: new Set(),
+              };
+          }
+          for (const v of info.sections) {
+              obj.sections.add(v);
+          }
+      }
+  }
+  const map2 = {};
+  for (const k in map1) {
+      const info = map1[k];
+      const day = info.day;
+      const sections = info.sections;
+      const name = info.name;
+      const teacher = info.teacher;
+      const position = info.position;
+      const key = hash(day, sections, name, teacher, position);
+      let obj = map2[key];
+      if (obj === undefined) {
+          map2[key] = obj = {
+              name,
+              teacher,
+              position,
+              day,
+              sections,
+              weeks: new Set(),
+          };
+      }
+      obj.weeks.add(info.week);
+  }
+
+  infos = [];
+  for (const k in map2) {
+      const v = map2[k];
+      v.weeks = setToArr(v.weeks);
+      v.sections = setToArr(v.sections);
+      infos.push(v);
+  }
+  return infos;
+}
+
+/*
+完整格式：
+课程代码<br>
+校区<br>
+<a onclick="openKckb()">课程名</a>课程类型（理论、实训等）<br>
+<a onclick="openJskb()">老师</a><br>
+周次<br>
+<a onclick="openCrkb()">教室</a><br>
+
+注意：
+课程代码、校区、课程类型与教室可能不存在
+*/
--- Provider.js
+++ Provider.js
@@ -1,24 +1,77 @@
-function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom = document) {//函数名不要动
-    // 以下可编辑
-    const ifrs = dom.getElementsByTagName("iframe");
-    const frs = dom.getElementsByTagName("frame");
-    if (ifrs.length) {
-      for (let i = 0; i < ifrs.length; i++) {
-        const dom = ifrs[i].contentDocument.body.parentElement;
-        iframeContent += scheduleHtmlProvider(iframeContent, frameContent, dom);
+/**
+ * 提取课程表
+ *
+ * @link https://github.com/moeshin/ai-schedule-chaoxing
+ * @param iframeContent {string}
+ * @param frameContent {string}
+ * @param dom {Document}
+ * @returns {string}
+ */
+async function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom = document) {
+  let element = document.querySelector('[name=username]');
+  if (element) {
+      const value = element.value;
+      if (typeof value === 'string' && value.toUpperCase() === 'PASS') {
+          return '<!-- PASS -->';
       }
-    }
-    if (frs.length) {
-      for (let i = 0; i < frs.length; i++) {
-        const dom = frs[i].contentDocument.body.parentElement;
-        frameContent += scheduleHtmlProvider(iframeContent, frameContent, dom);
-      }
-    }
-    if (!ifrs.length && !frs.length) {
-      return dom.querySelector('body').outerHTML
-    }
-    if (dom === document) {
-      return document.getElementsByTagName('html')[0].innerHTML + iframeContent + frameContent
-    }
-    return iframeContent + frameContent;
   }
+  const iframe = dom.querySelector('iframe[src$="/queryKbForXsd"]');
+  const path = '/admin/pkgl/xskb/queryKbForXsd';
+  if (location.pathname.replace(/\/+/g, '/')
+      .replace(/\/$/, '') === path) {
+      element = dom;
+  } else if (iframe) {
+      element = iframe.contentDocument;
+  } else {
+      await loadTool('AIScheduleTools');
+      const dom = await fetch(path)
+          .then(r => r.text())
+          .then(t => new DOMParser().parseFromString(t, 'text/html'));
+      const xhid = dom.querySelector('#xhid').value;
+      const xqdm = dom.querySelector('#xqdm').value;
+      const arr = [];
+      for (let option of dom.querySelectorAll('#xnxq1>option')) {
+          const value = option.value;
+          if (value) {
+              arr.push(value);
+          }
+      }
+      const xnxq = await AIScheduleSelect({
+          contentText: '学年学期',
+          selectList: arr,
+      });
+      return fetch('/admin/pkgl/xskb/sdpkkbList?' + new URLSearchParams({
+          xnxq,
+          xhid,
+          xqdm,
+      }))
+          .then(r => r.text())
+          .then(t => JSON.parse(t))
+          .then(data => data.map(info => {
+              const xq = info['xqmc'];
+              return {
+                  name: cleanTag(info['kcmc']) + getType(info['type']),
+                  teacher: cleanTag(info['tmc']),
+                  position: cleanTag(info['croommc']) + (xq ? `（${xq}）` : ''),
+                  sections: [info['djc']],
+                  weeks: info['zc'],
+                  day: info['xingqi'],
+              };
+          }))
+          .then(data => '<!-- JSON -->' + JSON.stringify(data));
+  }
+  return element.querySelector('table').outerHTML;
+}
+
+function cleanTag(str) {
+  console.log(str)
+  return new DOMParser().parseFromString(str, 'text/html').body.textContent.trim();
+}
+
+const TYPES = ['其他', '理论', '实验', '上机', '实践', '环节']
+function getType(type) {
+  if (type >= TYPES.length) {
+      type = 0;
+  }
+  return `（${TYPES[type]}）`;
+}
