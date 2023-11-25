5fa5bcefa65f5b118cd78911f9cb2e84163cc324
Sittymin <2890108976@qq.com>
2023-11-25 17:55:50 +0800

    实现冲突课表合并

--- Parser.js
+++ Parser.js
@@ -3,6 +3,7 @@ function scheduleHtmlParser(html) {
   let infos = JSON.parse(html);
   // 然后调用 format 函数来处理这些信息
   infos = format(infos);
+  infos = mergeConflictingCourses(infos);
   console.log('infos', infos);
   // 返回处理后的信息
   return infos;
@@ -30,4 +31,67 @@ function format(infos) {
 
   // 将累加器对象的值转换为数组
   return Object.values(mergedCourses);
+}
+
+
+function mergeConflictingCourses(courses) {
+
+  // 检查两个课程是否在某一周和某一节次冲突
+  function isConflict(course1, course2) {
+    return  course1.day === course2.day &&
+            course1.sections.some(section => course2.sections.includes(section)) &&
+            course1.weeks.some(week => course2.weeks.includes(week));
+  }
+
+  // 合并冲突的课程
+  function mergeCourses(course1, course2) {
+    // 找出两个课程都有的周数和节次
+    let commonWeeks = course1.weeks.filter(week => course2.weeks.includes(week));
+    let commonSections = course1.sections.filter(section => course2.sections.includes(section));
+
+    // 创建合并后的课程
+    let mergedCourse = {
+      name: `${course1.name}/${course2.name}`,
+      position: course1.position && course2.position ? `${course1.position}/${course2.position}` : '',
+      teacher: course1.teacher && course2.teacher ? `${course1.teacher}/${course2.teacher}` : '',
+      weeks: commonWeeks,
+      day: course1.day,
+      sections: commonSections
+    };
+
+    // 更新原课程的周数，移除与合并课程重叠的部分
+    course1.weeks = course1.weeks.filter(week => !commonWeeks.includes(week));
+    course2.weeks = course2.weeks.filter(week => !commonWeeks.includes(week));
+
+    return mergedCourse;
+  }
+
+  let mergedCourses = [];
+  let toMerge = [...courses];
+
+  while (toMerge.length > 0) {
+    let course = toMerge.shift();
+    let conflictIndex = toMerge.findIndex(c => isConflict(course, c));
+
+    if (conflictIndex !== -1) {
+      // 处理冲突
+      let conflictCourse = toMerge[conflictIndex];
+      let mergedCourse = mergeCourses(course, conflictCourse);
+
+      // 只有在课程依然有剩余周数时才将其添加回列表
+      if (course.weeks.length > 0) mergedCourses.push(course);
+      if (conflictCourse.weeks.length > 0) mergedCourses.push(conflictCourse);
+
+      // 添加合并后的课程
+      mergedCourses.push(mergedCourse);
+
+      // 移除已处理的冲突课程
+      toMerge.splice(conflictIndex, 1);
+    } else {
+      // 无冲突，直接添加
+      mergedCourses.push(course);
+    }
+  }
+
+  return mergedCourses;
 }
