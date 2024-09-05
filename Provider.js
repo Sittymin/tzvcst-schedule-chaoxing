8ecda9d57edd90cf3a47b9c0864d5f5d844fdab3
Sittymin <wu2890108976@gmail.com>
2024-09-05 20:13:36 +0800

    fix&feat: 修复课程时间获取，自动获取早中晚课程节数和当前学期

--- Provider.js
+++ Provider.js
@@ -1,54 +1,96 @@
-async function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom = document) {
-    //登录时的用户名
-    let element = dom.querySelector('[name="username"]');
-    //具体课表位置
-    const path = '/admin/pkgl/xskb/queryKbForXsd';
+async function scheduleHtmlProvider(
+  iframeContent = "",
+  frameContent = "",
+  dom = document,
+) {
+  await loadTool("AIScheduleTools");
+  if (!dom.querySelector(".userInfo")) {
+    await AIScheduleAlert("你还没登录, 估计不会按照预期执行");
+  } else {
+    await AIScheduleAlert("点击确认开始导入，不要重复按“一键导入”");
+  }
 
-    //调用小爱课程表
-    await loadTool('AIScheduleTools');
-    // 使用 fetch API 获取 path 路径的内容，然后将返回转换为文本后再转换为 HTML 文档
-    const contentDom = await fetch(path).then(res => res.text()).then(text => new DOMParser().parseFromString(text, 'text/html'));
-    // 从转换后的 HTML 文档中获取 xhid 和 xqdm 的值,还有遍历 xnxq 的 option 放入 arr
-    const xhid = contentDom.querySelector('#xhid').value;
-    const xqdm = contentDom.querySelector('#xqdm').value;
-    const arr = [];
-    for (let option of contentDom.querySelectorAll('#xnxq1>option')) {
-        const value = option.value;
-        if (value) {
-            arr.push(value);
-        }
+  // 具体课表位置
+  const path = "/admin/pkgl/xskb/queryKbForXsd";
+
+  // 调用小爱课程表
+  // 使用 fetch API 获取 path 路径的内容，然后将返回转换为文本后再转换为 HTML 文档
+  const contentDom = await fetch(path)
+    .then((res) => res.text())
+    .then((text) => new DOMParser().parseFromString(text, "text/html"));
+
+  // 从转换后的 HTML 文档中获取 xhid 和 xqdm 的值, 还有遍历 xnxq 的 option 放入 arr
+  const xhid = contentDom.querySelector("#xhid").value;
+  const xqdm = contentDom.querySelector("#xqdm").value;
+  const arr = [];
+  for (let option of contentDom.querySelectorAll("#xnxq1>option")) {
+    const value = option.value;
+    if (value) {
+      arr.push(value);
     }
-    //显示选择列表
-    const xnxq = await AIScheduleSelect({
-        contentText: '请选择学年学期',
-        selectList: arr,
-    });
+  }
 
-    //清理标签与最终返回部分
-    //获取课表具体数据
-    return fetch('/admin/pkgl/xskb/sdpkkbList?' + new URLSearchParams({
-        xnxq,
-        xhid,
-        xqdm,
-    }))
-    //处理返回数据
-        .then(res => res.text())
-        .then(text => JSON.parse(text))
-        .then(data => data.map(info => {
-            return {
-                name: cleanTag(info.kcmc),                  //课程名称
-                teacher: cleanTag(info.tmc),                //教师名称
-                position: cleanTag(info.croommc),           //上课地点
-                sections: info.djc,                         //节次
-                weeks: info.zcstr.split(",").map(Number),   //上课周数(分割并转换为数组)
-                day: info.xingqi,                           //星期
-            };
-        }))
-        //最终转换为JSON
-        .then(data => JSON.stringify(data));
+  // 获取当前学年学期
+  let xnxq = null;
+  // 下面不可以用自定义函数不然导入按钮没有反应
+  try {
+    const response = await fetch("/admin/xsd/xyjc/getXsjbxx?xhid=");
+    if (!response.ok) {
+      throw new Error("网络请求失败");
+    }
+    const result = await response.json();
+    if (result.ret === 0 && result.data && result.data.dqxnxq) {
+      console.log("当前学年学期:", result.data.dqxnxq);
+      xnxq = result.data.dqxnxq;
+    } else {
+      throw new Error("获取学期信息失败: " + result.msg);
+    }
+  } catch (error) {
+    console.error("请求当前学期信息出错:", error);
+  }
+
+  if (!xnxq) {
+    // 显示选择列表
+    xnxq = await AIScheduleSelect({
+      contentText: "请选择学年学期",
+      selectList: arr,
+    });
+  }
+
+  // 清理标签与最终返回部分
+  // 获取课表具体数据
+  return (
+    fetch(
+      "/admin/pkgl/xskb/sdpkkbList?" +
+        new URLSearchParams({
+          xnxq,
+          xhid,
+          xqdm,
+        }),
+    )
+      // 处理返回数据
+      .then((res) => res.text())
+      .then((text) => JSON.parse(text))
+      .then((data) =>
+        data.map((info) => {
+          return {
+            name: cleanTag(info.kcmc), // 课程名称
+            teacher: cleanTag(info.tmc), // 教师名称
+            position: cleanTag(info.croommc), // 上课地点
+            sections: info.djc, // 节次
+            weeks: info.zcstr.split(",").map(Number), // 上课周数(分割并转换为数组)
+            day: info.xingqi, // 星期
+          };
+        }),
+      )
+      // 最终转换为JSON
+      .then((data) => JSON.stringify(data))
+  );
 }
 
-//去除HTML标签
+// 去除HTML标签
 function cleanTag(str) {
-    return new DOMParser().parseFromString(str, 'text/html').body.textContent.trim();
+  return new DOMParser()
+    .parseFromString(str, "text/html")
+    .body.textContent.trim();
 }
--- Timer.js
+++ Timer.js
@@ -1,59 +1,90 @@
-//获取课表第一天日期
-function fetchCourseInfoForWeek(week) {
-  const data = { zc: week }; // 这里只需要设置周数
-
-  return fetch('https://tzvcst.jw.chaoxing.com/admin/getXqByZc', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    },
-    body: new URLSearchParams(data).toString() // 将对象转换为查询字符串格式
-  })
-  .then(response => response.json())
-  .catch(error => {
-    console.error('Error:', error);
-  });
+// 获取信息查询URL
+async function getCourseInfoUrl() {
+  let links = document.querySelectorAll(".radius a");
+  // 使用 for...of 循环遍历所有链接
+  for (let link of links) {
+    if (link.title === "信息查询") {
+      // 检查标题是否为 '信息查询'
+      console.log("成功获取到信息查询URL: " + link.href);
+      return link.href; // 返回符合条件的链接的完整 URL
+    }
+  }
+  // 如果没有找到符合条件的链接，执行以下代码
+  await loadTool("AIScheduleTools");
+  await AIScheduleAlert("时间相关URL获取失败，请联系维护同学修复");
+  return null;
 }
 
+// 获取课表按照周数的日期
+async function fetchDateInfoForWeek(week) {
+  const data = { zc: week }; // 这里只需要设置周数
 
-// 寻找默认选中的学年学期
-async function fetchSelectedYear() {
-  const path = '/admin/pkgl/xskb/queryKbForXsd';
-  const contentDom = await fetch(path).then(res => res.text()).then(text => new DOMParser().parseFromString(text, 'text/html'));
-
-  const selectElement = contentDom.getElementById('xnxq1');
-  const options = selectElement.querySelectorAll('option');
-  for (const option of options) {
-      if (option.selected) {
-        const selectedYear = option.value.split('-');
-        console.log('Selected year:', selectedYear);
-        return selectedYear;
+  return fetch("https://tzvcst.jw.chaoxing.com/admin/getXqByZc", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+    body: new URLSearchParams(data).toString(), // 将对象转换为查询字符串格式
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("网络响应错误");
       }
+      return response.json();
+    })
+    .then((jsonData) => {
+      console.log("获取到的一周日期:", jsonData.data);
+      return jsonData.data;
+    });
+}
+
+// 寻找当前学年学期
+async function getCurrentSemester() {
+  try {
+    const response = await fetch("/admin/xsd/xyjc/getXsjbxx?xhid=");
+    if (!response.ok) {
+      throw new Error("网络请求失败");
+    }
+    const result = await response.json();
+    if (result.ret === 0 && result.data && result.data.dqxnxq) {
+      console.log("当前学年学期:", result.data.dqxnxq);
+      return result.data.dqxnxq;
+    } else {
+      throw new Error("获取学期信息失败: " + result.msg);
+    }
+  } catch (error) {
+    console.error("请求当前学期信息出错:", error);
+    return null;
   }
 }
 
 // 获取默认课表第一天转换为时间戳
 async function toTimestamp() {
-  const selectedYear = await fetchSelectedYear(); // 等待fetchSelectedYear函数的异步操作完成
-  console.log('Selected year in toTimestamp:', selectedYear);
-  const year = selectedYear[2] === '1' ? selectedYear[0] : selectedYear[1];
-  const data = await fetchCourseInfoForWeek(1); // 等待fetchCourseInfoForWeek函数的异步操作完成
-  console.log('Data in toTimestamp:', data);
-  // data.data才是真正的课程信息
-  if (data && data.data && data.data.length > 0) {
-    const firstDay = data.data[0]; // 第一天的课程信息
-    const [month, day] = firstDay.date.split('-').map(Number);
-    const timestamp = Date.UTC(year, month - 1, day);
-    return timestamp;
-  } else {
+  const url = await getCourseInfoUrl();
+  if (!url) {
+    console.error("getCourseInfoUrl() 未成功获取到 URL");
     return null;
   }
+
+  const semester = await getCurrentSemester();
+  if (!semester) {
+    console.error("getSemester() 未成功获取到当前学年学期");
+    return null;
+  }
+  const semesterArray = semester.split("-");
+  const year = semesterArray[2] === "1" ? semesterArray[0] : semesterArray[1];
+  console.log("分割后的当前年:", year);
+
+  const data = await fetchDateInfoForWeek(1);
+  if (!data || data.length === 0) {
+    console.error("按照周数获取到的日期没有");
+    return null;
+  }
+  const firstDay = data[0];
+  const [month, day] = firstDay.date.split("-").map(Number);
+  const timestamp = Date.UTC(year, month - 1, day);
+  return timestamp;
 }
-
-
-
-
-
 
 function findMaxWeek(courses) {
   return courses.reduce((max, course) => {
@@ -64,30 +95,79 @@ function findMaxWeek(courses) {
   }, 0); // 初始值设置为0
 }
 
-async function scheduleTimer({
-parserRes
-} = {}) {
+async function fetchCourseTimes() {
+  try {
+    const url =
+      "https://tzvcst.jw.chaoxing.com/admin/system/zy/xlgl/selectJxzxsjXq/" +
+      (await getCurrentSemester());
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("网络请求失败");
+    }
+    const result = await response.json();
+    if (result && result.list && result.list.length > 0) {
+      const sections = result.list.map((item) => ({
+        section: parseInt(item.jc, 10),
+        startTime: item.kssj,
+        endTime: item.jssj,
+      }));
+
+      console.log("转换后的课程时间:", sections);
+      return sections;
+    } else {
+      throw new Error("获取数据失败或数据为空");
+    }
+  } catch (error) {
+    console.error("课程时间获取失败:", error);
+    return null;
+  }
+}
+
+// 分类
+function categorizeSections(sections) {
+  let forenoon = 0,
+    afternoon = 0,
+    night = 0;
+  sections.forEach((section) => {
+    const startHour = parseInt(section.startTime.split(":")[0], 10);
+    if (startHour < 12) {
+      forenoon++;
+    } else if (startHour < 18) {
+      afternoon++;
+    } else {
+      night++;
+    }
+  });
+  return { forenoon, afternoon, night };
+}
+
+async function scheduleTimer({ parserRes } = {}) {
   const obj = {};
   if (parserRes) {
-      obj.totalWeek = findMaxWeek(parserRes);
+    obj.totalWeek = findMaxWeek(parserRes);
   }
 
-  obj.forenoon = 5;
-  obj.afternoon = 4;
-  obj.night = 3;
-
-  obj.startSemester = (await toTimestamp()).toString();
+  // 开学时间（时间戳）
+  const startSemester = await toTimestamp();
+  if (startSemester === null) {
+    console.error("无法获取开学时间");
+    return null;
+  }
+  obj.startSemester = startSemester.toString();
 
   // 从网页中获取课程时间
-  obj.sections = Array.from(document.querySelectorAll('.td-course-time')).flatMap(element => {
-    const courseNumElement = element.querySelector('.course-num');
-    const section = parseInt(courseNumElement.textContent, 10);
-    if (isNaN(section)) {
-      return []; // 如果section不是数字，返回一个空数组，flatMap会将其展平，相当于跳过这个元素
-    }
-    const startTime = element.querySelector('.time-start').textContent;
-    const endTime = element.querySelector('.time-end').textContent;
-    return [{ section, startTime, endTime }];
-  });
+  const sections = await fetchCourseTimes();
+  obj.sections = sections;
+
+  // 课程节数
+  if (sections) {
+    const { forenoon, afternoon, night } = categorizeSections(sections);
+    obj.forenoon = forenoon;
+    obj.afternoon = afternoon;
+    obj.night = night;
+  } else {
+    console.error("无法获取课程时间");
+  }
+
   return obj;
 }
